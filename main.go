71f1d55c85c77fd0007b758922bf47a19ed6543e
vnSasa <ol.yaremechko@gmail.com>
2022-07-31 18:57:27 +0300

    added read data json

--- main.go
+++ main.go
@@ -1,14 +1,61 @@
 package main
 
+import (
+	"encoding/json"
+	"fmt"
+	"os"
+	"time"
+)
+
 type Trains []Train
 
 type Train struct {
-	TrainID            int       
-	DepartureStationID int       
-	ArrivalStationID   int       
-	Price              float32   
-	ArrivalTime        time.Time 
-	DepartureTime      time.Time
+	TrainID            int			`json:"trainId"` 
+	DepartureStationID int			`json:"departureStationID"`
+	ArrivalStationID   int			`json:"arrivalStationId"`
+	Price              float32		`json:"price"`
+	ArrivalTime        time.Time	`json:"arrivalTime"`
+	DepartureTime      time.Time	`json:"departureTime"`
+}
+
+func readDataJSON() (data string) {
+	jsonFile, err := os.ReadFile("data.json")
+	if err != nil {
+		fmt.Println(err)
+	}
+	err = json.Unmarshal(jsonFile, &data)
+	if err != nil {
+		fmt.Println(err)
+	}
+	return data
+}
+
+func (t *Train) UnmarshalJSON(data []byte)error {
+	var byteValue map[string]interface{}
+	err := json.Unmarshal(data, &byteValue)
+	if err != nil {
+		return err
+	}
+
+	id, _ := byteValue["trainId"].(float64)
+	t.TrainID = int(id)
+
+	depStationId, _ := byteValue["departureStationID"].(float64)
+	t.DepartureStationID = int(depStationId)
+
+	arrStationId, _ := byteValue["arrivalStationId"].(float64)
+	t.ArrivalStationID = int(arrStationId)
+
+	price, _ := byteValue["price"].(float64)
+	t.Price = float32(price)
+
+	arrTime, _ := byteValue["arrivalTime"].(string)
+	t.ArrivalTime, _ = time.Parse("15:04:05", arrTime)
+
+	depTime, _ := byteValue["departureTime"].(string)
+	t.DepartureTime, _ = time.Parse("15:04:05", depTime)
+
+	return nil
 }
 
 func main() {
